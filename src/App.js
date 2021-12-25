2c957d69a6b39f127212187f4b2b4c1c541a9b79
lucas <mr.pine97@gmail.com>
2021-12-25 16:15:25 -0300

    adiciona hashrouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import { HashRouter } from "react-router-dom";
+import RouteWays from "./RouteWays";
+
 function App() {
   return (
-    <div className="App">
-      test
-    </div>
+    <HashRouter>
+      <RouteWays />
+    </HashRouter>
   );
 }
 
